8300cdde33a7428e276bd9b11a948fe0207dfb23
thme80 <thme808@gmail.com>
2020-12-11 23:24:30 +0900

    create user account with email and pw

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import {authService} from 'myBase';
 
 function App() {
-  console.log(authService.currentUser);
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  useEffect(()=> {
+    authService.onAuthStateChanged((user) => {
+      if(user){
+        setIsLoggedIn(true);       
+      }else{
+        setIsLoggedIn(false);
+      }
+      setInit(true) ;
+    });
+  }, []);
   return (
     <AppRouter isLoggedIn={isLoggedIn} />
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { authService } from "../myBase";
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAcount] = useState(true);
+
+    const onChange = (e) => {
+        const {target : {name, value}} = e;
+        if(name === "email") {
+            setEmail(value);
+        }else if(name === "password"){
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let data;
+            if(newAccount){
+                //create account
+                data = await authService.createUserWithEmailAndPassword(
+                    email, password
+                );
+            }else{
+                //log in
+                data = await authService.signInAndRetrieveDataWithCredential(
+                    email, password
+                );
+            }
+            console.log(data);
+        }catch(error){
+            console.log(error);
+        }
+    };
+
+    return (
+        <div>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} type="text" placeholder="Email" name="email" required value={email} />
+            <input onChange={onChange} type="password" placeholder="Password" name="password" required value={password} />
+            <input type="submit" value={newAccount ? "create account" : "log in"} />
+        </form>
+        <div>
+            <button>Continue with Google</button>
+            <button>Continue with Github</button>
+        </div>
+        </div>
+    )
+}
  
 export default Auth;
